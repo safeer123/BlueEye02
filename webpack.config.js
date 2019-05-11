885c4602203674327875a578d858f46b64a53b41
Safeer Chonengal <safeer2c@gmail.com>
2019-05-11 23:21:48 +0530

    copy redirects to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,57 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const WebpackMerge = require('webpack-merge');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const imageminMozjpeg = require('imagemin-mozjpeg');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const WebpackMerge = require("webpack-merge");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const imageminMozjpeg = require("imagemin-mozjpeg");
 
-
-const API_SERVER_URL = 'http://is-gnv-hpapp01:9701/';
+const API_SERVER_URL = "http://is-gnv-hpapp01:9701/";
 
 // Development Configuration
 const devConfig = () => ({
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: 'dist',
+    contentBase: "dist",
     compress: true,
     port: process.env.port,
-    stats: 'errors-only',
+    stats: "errors-only",
     open: true,
     inline: true,
     historyApiFallback: {
-      disableDotRule: true,
+      disableDotRule: true
     },
     proxy: {
-      '/api': {
+      "/api": {
         // Rest API Server
         target: API_SERVER_URL,
-        changeOrigin: true,
+        changeOrigin: true
       },
-      '/token': {
+      "/token": {
         // auth proxy
-        target: API_SERVER_URL,
+        target: API_SERVER_URL
       },
-      '/auth': {
-        target: API_SERVER_URL,
+      "/auth": {
+        target: API_SERVER_URL
       },
-      '/notifications': {
+      "/notifications": {
         target: API_SERVER_URL,
         changeOrigin: true,
-        ws: true,
-      },
-    },
-  },
+        ws: true
+      }
+    }
+  }
 });
 
 // Production Configuration
 const prodConfig = () => ({
-  devtool: 'source-map',
+  devtool: "source-map",
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
@@ -59,139 +59,142 @@ const prodConfig = () => ({
         // parallel: true,
         // sourceMap: true // set to true if you want JS source maps
       }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
   plugins: [
     // new webpack.HotModuleReplacementPlugin(),
     //  new BundleAnalyzerPlugin()
-  ],
+  ]
 });
 
 // Common configuration for Prod and Dev
 const commonConfig = () => ({
   output: {
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: "[name].[hash].js",
+    chunkFilename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
   },
   node: {
-    fs: 'empty',
+    fs: "empty"
   },
   module: {
     rules: [
-    //   {
-    //     type: 'javascript/auto',
-    //     test: /\.json$/,
-    //     use: [
-    //         {
-    //           loader: 'file-loader',
-    //           options: {
-    //               name: "./externalConfig/[name].[ext]"
-    //           }
-    //         }
-    //     ]
-    // },
+      //   {
+      //     type: 'javascript/auto',
+      //     test: /\.json$/,
+      //     use: [
+      //         {
+      //           loader: 'file-loader',
+      //           options: {
+      //               name: "./externalConfig/[name].[ext]"
+      //           }
+      //         }
+      //     ]
+      // },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: "babel-loader"
+        }
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-            options: { minimize: true },
-          },
-        ],
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
-              name: 'fonts/[name].[ext]?[hash]',
-            },
-          },
-        ],
+              name: "fonts/[name].[ext]?[hash]"
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
-              name: 'assets/images/[name].[ext]?[hash]',
-            },
-          },
-        ],
+              name: "assets/images/[name].[ext]?[hash]"
+            }
+          }
+        ]
       },
       {
         test: /\.scss$/,
         use: [
-          'style-loader', // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
-        ],
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
       },
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
+      template: "./public/index.html",
+      filename: "./index.html"
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
     new CopyWebpackPlugin([
       {
         from: `${__dirname}/src/app/assets`,
-        to: `${__dirname}/dist/app/assets/`,
+        to: `${__dirname}/dist/app/assets/`
       },
+      {
+        from: `${__dirname}/public/_redirects`,
+        to: `${__dirname}/`
+      }
     ]),
-    new CleanWebpackPlugin(['dist'], {
+    new CleanWebpackPlugin(["dist"], {
       root: path.resolve(__dirname),
       verbose: true,
       dry: false,
-      exclude: ['node_modules'],
+      exclude: ["node_modules"]
     }),
     new webpack.ProvidePlugin({
-      ReactDOM: 'react-dom',
-      React: 'react',
-      $: 'jquery',
-      jQuery: 'jquery',
+      ReactDOM: "react-dom",
+      React: "react",
+      $: "jquery",
+      jQuery: "jquery"
     }),
     new ImageminPlugin({
-      pngquant: ({ quality: '50' }),
-      plugins: [imageminMozjpeg({ quality: '50' })],
-    }),
-
-  ],
+      pngquant: { quality: "50" },
+      plugins: [imageminMozjpeg({ quality: "50" })]
+    })
+  ]
 });
 
-module.exports = (env) => {
-  if (env.prod === 'true') {
+module.exports = env => {
+  if (env.prod === "true") {
     return WebpackMerge(commonConfig(), prodConfig());
   }
   return WebpackMerge(commonConfig(), devConfig());
